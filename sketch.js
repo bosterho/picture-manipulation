f0388e32406fa5d397dbead63975db2ea0a0f9d0
bosterho <48608512+bosterho@users.noreply.github.com>
2022-07-14 13:32:19 -0500

    made the circles smaller

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let img
-function preload(){
+function preload() {
   img = loadImage("assets/garden.jpg")
 }
 
@@ -9,9 +9,21 @@ function setup() {
 }
 
 function draw() {
-  let v = createVector(random(width), random(height))
-  let px = get(v.x, v.y)
-  fill(px)
-  noStroke()
-  circle(v.x, v.y, 10)
+  for (let i = 0; i < 1000; i++) {
+    let v = createVector(random(width), random(height))
+    let px = get(v.x, v.y)
+    fill(px)
+    noStroke()
+    circle(v.x, v.y, 2)
+  }
+}
+
+function mouseDragged(){
+  // for (let i = 0; i < 10; i++) {
+  //   let v = createVector(mouseX + random(-10, 10), mouseY + random(-10, 10))
+  //   let px = get(v.x, v.y)
+  //   fill(px)
+  //   noStroke()
+  //   circle(v.x, v.y, 5)
+  // }
 }
